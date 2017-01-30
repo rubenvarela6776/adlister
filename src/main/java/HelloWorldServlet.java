9d04c4e8cd7c3b9a26841be41c4b12c4557a35c4
rubenvarela <rubenv6776@gmail.com>
2017-01-30 15:29:47 -0600

    Added counter.

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,12 +1,9 @@
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import static java.lang.System.out;
 import java.io.*;
 import javax.servlet.*;
-import javax.servlet.http.*;
 
 
 
@@ -14,6 +11,9 @@ import javax.servlet.http.*;
 // url patterns is a set of paths
 @WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
 public class HelloWorldServlet extends HttpServlet {
+
+    private int counter = 0;
+
     protected void doGet(
         HttpServletRequest request,
         HttpServletResponse response
@@ -31,18 +31,7 @@ public class HelloWorldServlet extends HttpServlet {
         }
 
 
-        HttpSession session = request.getSession(true);
-        int counter = 0;
-        Integer ival = (Integer) session.getValue("knownuser");
-        if (ival==null) {
-            ival = new Integer(1);
-            counter++;
-            session.putValue("knownuser", ival);
-        }
-
-        System.out.println(counter);
-
-        PrintWriter out2 = response.getWriter();
-        out2.println(counter);
+        counter +=1;
+        response.getWriter().println("The count is: " + counter);
     }
 }
